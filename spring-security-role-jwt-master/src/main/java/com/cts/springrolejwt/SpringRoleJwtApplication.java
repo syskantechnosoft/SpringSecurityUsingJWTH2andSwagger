f280ac85911abda58a19488dcfe8521c3e43dfde
Siva OS <syskantechnosoft@users.noreply.github.com>
2022-02-23 04:20:29 -0500

    Add files via upload

---
+++ spring-security-role-jwt-master/src/main/java/com/cts/springrolejwt/SpringRoleJwtApplication.java
@@ -0,0 +1,16 @@
+package com.cts.springrolejwt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+@SpringBootApplication
+@EnableSwagger2
+public class SpringRoleJwtApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringRoleJwtApplication.class, args);
+	}
+
+}
